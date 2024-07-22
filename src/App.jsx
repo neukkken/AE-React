b8c1577d6c8697c0e6748b01b0bdfa8e7aa66a18
sebasbn29 <sebastianburbano06@gmail.com>
2024-07-22 14:32:57 -0500

    nuevo template, vamos a usar COREUI

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CFormInput, CForm } from '@coreui/react'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function App() {
+    return (
+      
+  )
+}
