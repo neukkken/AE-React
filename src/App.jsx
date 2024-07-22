e13f68ad7fa47b33cb7e3602bd312135f16dab67
sebasbn29 <sebastianburbano06@gmail.com>
2024-07-22 14:47:57 -0500

    rutas y eso

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { CFormInput, CForm } from '@coreui/react'
 import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home/>}
+  ])
+
+  return routes
+}
 
 export default function App() {
     return (
-      
+      <>
+        <BrowserRouter>
+          <AppRoutes/>
+        </BrowserRouter>
+      </>
   )
 }
