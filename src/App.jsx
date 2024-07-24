f602966cd57acee707ced013d338af1787bed97f
JOSSIE2463 <jarandapechene@gmail.com>
2024-07-24 14:09:21 -0500

    agregar campos adicionales

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home'
 import "../style.css"
+import SubirProyecto from './Pages/usuario/SubirProyecto'
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    { path: "/", element: <Home/>}
+    { path: "/", element: <Home/> },
+    { path: "/subir-proyecto", element: <SubirProyecto /> }
   ])
-
+  
   return routes
 }
 
 export default function App() {
-    return (
-      <>
-        <BrowserRouter>
-          <AppRoutes/>
-        </BrowserRouter>
-      </>
+  return (
+    <>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </>
   )
 }
